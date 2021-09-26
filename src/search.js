ca645c55fbd23744f52128aea747a03765b42ffe
withercraft303 <lsntao.withercraft303@gmail.com>
2021-09-26 10:19:43 -0400

    fixed collapsables

--- src/search.js
+++ src/search.js
@@ -1,37 +1,5 @@
-function searchFunction() {
-    var input, filter, ul, li, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    group = ul.getElementsByClassName("group");
-    for (i = 0; i < group.length; i++) {
-        txtValue = group[i].getElementsByTagName("button")[0].innerHTML;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            group[i].style.display = "";
-        } else {
-            group[i].style.display = "none";
-        }
-    }
-}
+function onLoad() {
 
-function buttonFunction() {
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-            content.style.display = "none";
-            } else {
-            content.style.display = "block";
-            }
-    });
-} 
-}
-
-function createParks() {
     var url = "https://raw.githubusercontent.com/RiceFarmer1/BorderHacks/main/src/data/data.csv";
 
     var request = new XMLHttpRequest();  
@@ -52,7 +20,6 @@ function createParks() {
         const btn = document.createElement("button");
         btn.innerHTML = csvData[i][1];
         btn.className = "collapsible";
-        btn.onclick = buttonFunction();
         group.className = "group";
         
         const div = document.createElement("div");
@@ -70,5 +37,58 @@ function createParks() {
         group.appendChild(btn);
         group.appendChild(div);
         parent.appendChild(group);
+    }
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+                var a = this.classList.toggle("active");
+                console.log(a);
+                var content = this.nextElementSibling;
+                if (content.style.display === "block") {
+                content.style.display = "none";
+                } else {
+                content.style.display = "block";
+                }
+        });
+    } //cite: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible
+}
+
+function searchFunction() {
+    var input = document.getElementById("myInput");
+    var search = input.value.toUpperCase();
+    var ul = document.getElementById("myUL");
+    var group = ul.getElementsByClassName("group");
+
+    var i, txtValue;
+    for (i = 0; i < group.length; i++) {
+        txtValue = group[i].getElementsByTagName("button")[0].innerHTML;
+        if (txtValue.toUpperCase().indexOf(search) > -1) {
+            group[i].style.display = "";
+        } else {
+            group[i].style.display = "none";
+        }
+    }
+}
+
+function collapseAll() {
+    var group = document.getElementById("myUL").getElementsByClassName("group");
+    var button;
+    for (var i = 0; i < group.length; i++){
+        button = group[i].getElementsByTagName("button")[0];
+        button.classList.toggle('active', false)
+        button.nextElementSibling.style.display = "none";
+    }
+}
+
+function openAll() {
+    var group = document.getElementById("myUL").getElementsByClassName("group");
+    var button;
+    for (var i = 0; i < group.length; i++){
+        button = group[i].getElementsByTagName("button")[0];
+        button.classList.toggle('active', true)
+        button.nextElementSibling.style.display = "";
     }
 }

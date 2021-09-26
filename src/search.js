1c9dbed2f52ec2b1ea3502c9f326a7e94af3dec4
withercraft303 <lsntao.withercraft303@gmail.com>
2021-09-25 22:58:53 -0400

    database stuff

--- src/search.js
+++ src/search.js
@@ -15,16 +15,40 @@ function myFunction() {
     }
 }
 function createParks() {
-    const parent = document.getElementById("div1");
-    const child = document.getElementById("p1");
+    
+    for (i=0;i<3;i++){
+        const parent = document.getElementById("myUL");
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const node = document.createTextNode("park name here");
+        a.appendChild(node);
+        li.appendChild(a);
+        parent.appendChild(li);
+    }
+}
+function test() {
+    function readTextFile(file){
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function ()
+        {
+            if(rawFile.readyState === 4)
+            {
+                if(rawFile.status === 200 || rawFile.status == 0)
+                {
+                    var allText = rawFile.responseText;
+                    alert(allText);
+                }
+            }
+        }
+        rawFile.send(null);
+    } // source: https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file
 
-    const li = document,createElement("li");
-    const a = document.createElement("a");
-    const node = document.createTextNode("park name here");
-    para.appendChild(node);
+    const csvFile = readTextFile("https://raw.githubusercontent.com/RiceFarmer1/BorderHacks/main/src/Fundy_NP_Forest_TrailConditionIndex_2014_data.csv");
+    console.log(typeof(csvFile))
+    //const reader = new FileReader();
 
-    const element = document.getElementById("div1");
-    const child = document.getElementById("p1");
-    element.insertBefore(para, child);
+    //const text = e.target.result;
+    //document.write(text);
 
 }
